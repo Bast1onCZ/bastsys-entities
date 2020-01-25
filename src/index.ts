852d96ac41abfc163ce0c396e336dcb786da2b2a
klobaska <mir.klobaska@seznam.cz>
2020-01-25 09:24:23 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 export {default as EntityProvider} from './components/EntityProvider'
-export {default as UpdateMethodType} from './components/syncField/SyncFieldType'
+export {default as UpdateMethodType} from './components/EntityProvider/UpdateMethodType'
 
 export {default as useStringField} from './hooks/entityField/useStringField'
 export {default as useNumberField} from './hooks/entityField/useNumberField'
