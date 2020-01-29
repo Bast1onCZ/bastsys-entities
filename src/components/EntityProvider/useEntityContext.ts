d172482023314905a6e04757d38c82ccf703ac46
klobaska <mir.klobaska@seznam.cz>
2020-01-29 18:51:09 +0100

    useEntityContext.ts added entity type param

--- src/components/EntityProvider/useEntityContext.ts
+++ src/components/EntityProvider/useEntityContext.ts
@@ -1,7 +1,8 @@
 import EditableEntityContext, {EditableEntityContextValue} from './EditableEntityContext'
 import {useContext} from 'react'
+import {Entity} from '../../api/types'
 
-export default function useEntityContext(): EditableEntityContextValue<any> {
+export default function useEntityContext<E extends Entity>(): EditableEntityContextValue<E> {
     const ctx = useContext(EditableEntityContext)
     if(!ctx) {
         throw new Error('Entity context does not wrap this component')
