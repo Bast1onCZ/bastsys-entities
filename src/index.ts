832c3894c33a3fa52f2e05a4dc4624f498879a5b
klobaska <mir.klobaska@seznam.cz>
2020-02-22 11:21:17 +0100

    added useEntityValue to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
 export {default as EntityProvider} from './components/EntityProvider'
 export {default as UpdateMethodType} from './components/EntityProvider/UpdateMethodType'
 
+export {default as useEntityValue} from './hooks/entityField/utils/useEntityValue'
+
 export {default as useStringField} from './hooks/entityField/useStringField'
 export {default as useNumberField} from './hooks/entityField/useNumberField'
 export {default as useSelectField} from './hooks/entityField/useSelectField'
