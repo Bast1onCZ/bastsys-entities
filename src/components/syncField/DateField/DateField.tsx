f256b557ae7b2fcc7345e31a368add8fc4dd38b1
klobaska <mir.klobaska@seznam.cz>
2020-03-23 17:53:44 +0100

    disabled and hidden option for DateField.tsx

--- src/components/syncField/DateField/DateField.tsx
+++ src/components/syncField/DateField/DateField.tsx
@@ -1,13 +1,13 @@
-import {forwardRef, memo, useCallback, useRef} from 'react'
+import React, {forwardRef, KeyboardEvent, memo, useCallback, useRef} from 'react'
 import {SyncFieldDefinition} from '../../../hooks/entityField/types'
 import {useStringField} from '../../../index'
 import {SyncFieldReference} from '../../EntityProvider/useSyncFieldImperativeHandle'
 import {KeyboardDatePicker} from '@material-ui/pickers'
-import React, {KeyboardEvent} from 'react'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 export interface DateFieldProps extends SyncFieldDefinition {
-
+    hidden?: boolean
+    disabled?: boolean
 }
 
 const DateField = forwardRef<SyncFieldReference, DateFieldProps>((props, ref) => {
@@ -18,31 +18,33 @@ const DateField = forwardRef<SyncFieldReference, DateFieldProps>((props, ref) =>
         changeTempValue(date.toString())
     }, [changeTempValue])
     const handleKeyPress = useCallback((e: KeyboardEvent<HTMLDivElement>) => {
-        if(e.key === 'Enter') {
+        if (e.key === 'Enter') {
             inputRef.current?.blur()
         }
     }, [])
     const handleBlur = useCallback(() => {
-        if(!validation.hasError) {
+        if (!validation.hasError) {
             confirmChange()
         }
     }, [validation.hasError, confirmChange])
 
-    return (
-        <KeyboardDatePicker
-            ref={inputRef}
-            value={value}
-            onChange={handleTempChange}
-            label={props.label}
-            onKeyPress={handleKeyPress}
-            onBlur={handleBlur}
-            format="MM/dd/yyyy"
-            error={validation.hasError}
-            helperText={validation.error}
-            disabled={isSyncing}
-            keyboardIcon={isSyncing ? <CircularProgress size={20} color="secondary" /> : undefined}
-        />
-    )
+    return props.hidden
+        ? null
+        : (
+            <KeyboardDatePicker
+                ref={inputRef}
+                value={value}
+                onChange={handleTempChange}
+                label={props.label}
+                onKeyPress={handleKeyPress}
+                onBlur={handleBlur}
+                format="MM/dd/yyyy"
+                error={validation.hasError}
+                helperText={validation.error}
+                disabled={isSyncing || props.disabled}
+                keyboardIcon={isSyncing ? <CircularProgress size={20} color="secondary"/> : undefined}
+            />
+        )
 })
 
 export default memo(DateField)
